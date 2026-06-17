9541e093dbbf5996e84f31a47d9b8f749c43116d
NN <Pongsapat>
2026-06-17 10:06:02 +0700

    ticket fix

--- WX/ticket.py
+++ WX/ticket.py
@@ -2,6 +2,12 @@ import sys
 
 def calculate_ticket_price(age):
     # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
+    if age < 12:
+        return 120
+    if age >= 12 and age <= 60:
+        return 200
+    if age > 60:
+        return 150
     pass
     # --------------------------------------------------------
 
@@ -12,7 +18,7 @@ def main():
         result = calculate_ticket_price(test_age)
         print(result)
     else:
-        test_age = 25
+        test_age = int(input("Enter age: "))
         result = calculate_ticket_price(test_age)
         print(f"Age: {test_age} -> Ticket Price: {result} Baht")
 
