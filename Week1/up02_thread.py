0fc4cb1bdd537617fccedf09987fd91d503a258b
NN <Pongsapat>
2026-06-24 11:39:59 +0700

    fix start up02

--- Week1/up02_thread.py
+++ Week1/up02_thread.py
@@ -32,10 +32,11 @@ def main():
             name=f"Thread-{customer}"
         )
         threads.append(t)
+        t.start()
 
     # สั่งให้ทุก Thread เริ่มทำงาน
-    for t in threads:
-        t.start()
+    #for t in threads:
+    #    t.start()
 
     # รอให้ทุก Thread ทำงานเสร็จ
     for t in threads:
