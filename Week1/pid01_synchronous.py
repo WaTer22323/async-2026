43ce282a3e29ffc9c081a7fd1f43cfa5dbf0d12e
NN <Pongsapat>
2026-06-24 10:37:17 +0700

    pid01

--- Week1/pid01_synchronous.py
+++ Week1/pid01_synchronous.py
@@ -4,11 +4,31 @@ import threading
 
 # ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คนแบบซิงโครนัส
 def make_coffee(customer_name):
-    pass
+    # ดึง PID และ Thread ID ออกมาดู
+    pid = os.getpid()
+    thread_id = threading.current_thread().native_id #  ดึง Thread ID ของ Thread ปัจจุบัน
+    thread_name = threading.current_thread().name # ดึงชื่อ Thread ปัจจุบัน
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Thread Name: {thread_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...") 
+    sleep(5)  # บล็อกการทำงานของ Thread นี้ไว้ 5 วินาทีเต็ม
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Thread Name: {thread_name}] ลูกค้า {customer_name}: ได้รับกาแฟแล้ว!")
+
 
 def main():
-    pass
+    queue = ['A', 'B', 'C'] # ลิสต์ลูกค้าที่เข้ามาในคิว (ทีละคน)
+    main_pid = os.getpid() # ดึง PID ของ Process หลัก
+    main_tid = threading.current_thread().native_id # ดึง Thread ID ของ Thread หลัก
+
+    print(f"{ctime()} | [Main PID: {main_pid}] [Main TID: {main_tid}] === เริ่มระบบจำลองตู้กาแฟแบบ Synchronous ===")
+    start_time = time() # บันทึกเวลาที่เริ่มทำงาน
+
+    # ลูปทำงานตามลำดับคิวเดียว (ทีละคน)
+    for customer in queue:
+        make_coffee(customer)
+
+    duration = time() - start_time
+    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:0.2f} วินาที")
+
 
 if __name__ == "__main__":
     main()
-    
