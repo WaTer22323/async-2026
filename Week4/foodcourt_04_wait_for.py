7fa2cf66f80e4e9b23bad83c20010be598eee7f8
NN <Pongsapat>
2026-07-15 10:21:24 +0700

    33

--- Week4/foodcourt_04_wait_for.py
+++ Week4/foodcourt_04_wait_for.py
@@ -6,22 +6,18 @@ from food_utils import send_order_to_kitchen
 
 async def main():
     MY_STUDENT_ID = "6720301004"
-    print(f"{ctime()} | --- [Task 4] ")
+    print(f"{ctime()} | --- [Task 4] Practice using wait_for to handle timeouts ---")
 
     try:
-        #
-        print(f"{ctime()} | --- [System] Order Sent: ")
-        results = await asyncio.wait_for(
+        print(f"{ctime()} | [System] Order sent. Monitoring 2.0s timeout limit...")
+        result = await asyncio.wait_for(
             send_order_to_kitchen(MY_STUDENT_ID, "steak", "T-Bone Steak"),
             timeout=2.0
         )
-        print(f"{ctime()} | System Response: {results}")
+        print(f"{ctime()} | System Response: {result}")
 
     except asyncio.TimeoutError:
-        #
-        print(f"{ctime()} | TimeoutError: The order took too long to complete.")
-
-
+        print(f"{ctime()} | Timeout occurred: Steak took too long! Leaving the food court now.")
 
 
 if __name__ == "__main__":
